263e6b8282d020d5ccaf64d169edcbc4275de787
dudemelo <dudemelo@gmail.com>
2015-12-17 15:52:13 -0300

    Fix git log format test

--- tests/GitAnalytics/Command/Git/GitLogCommandTest.php
+++ tests/GitAnalytics/Command/Git/GitLogCommandTest.php
@@ -18,8 +18,8 @@ final class GitLogCommandTest extends \PHPUnit_Framework_TestCase
 
     public function testFormat()
     {
-        $this->log->format('TEST');
+        $this->log->format('%ad');
         $output = $this->log->execute();
-        $this->assertEquals('TEST', $output);
+        $this->assertTrue(1 === preg_match('/^(\w{3}\s){2}\d{2}\s(\d{2}(\:|\s)){3}\d{4}\s\-\d{4}$/im', $output));
     }
 }
